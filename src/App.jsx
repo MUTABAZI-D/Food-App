5dff7671e4814f12f81a8980d204541bafee6816
MUTABAZI Dieudonne <mudonne18@gmail.com>
2024-12-06 19:01:48 +0200

    added styles for better responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ function App() {
   const [foodData, setFoodData] = useState([]);
   const [foodId, setFoodId] = useState("656329");
   return (
-    <>
+    <div>
       <Nav></Nav>
       <Search foodData={foodData} setFoodData={setFoodData}></Search>
       <div className="grid grid-cols-3 md:grid-cols-2 md:mx-5 md:gap-5">
         <FoodList setFoodId={setFoodId} foodData={foodData} />
         <FoodDetails foodId={foodId} />
       </div>
-    </>
+    </div>
   );
 }
 
